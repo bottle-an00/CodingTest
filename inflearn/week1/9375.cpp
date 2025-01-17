ada1aec55b645842ab5a486140c5d1b7dbb511f3
jba <sjba10815@gmail.com>
2025-01-17 20:15:21 +0900

    20250117

--- inflearn/week1/9375.cpp
+++ inflearn/week1/9375.cpp
@@ -5,16 +5,18 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);cout.tie(nullptr);
 
-    int N{};
+    int N;
     cin >> N; 
 
     for(int i = 0; i <N; ++i){
-        int M{};
+        int M;
         cin >> M;
+        cin.ignore();
         map<string,vector<string>> mp;
         for(int j = 0 ; j < M; ++j){
             string input;
             getline(cin,input);
+
             auto pos = input.find(" ");
             string name = input.substr(0,pos);
             string category = input.substr(pos+1,input.size());
@@ -24,7 +26,7 @@ int main(){
 
         int res{1};
         for(auto iter = mp.begin(); iter !=mp.end(); ++iter){
-            res *= iter->second.size();
+            res *= iter->second.size() + 1;
         }
         cout << --res << endl;
     }
