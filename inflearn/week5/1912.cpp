2606f8c84fd6ef29941032a8a09c09d9df957da2
bottle-an00 <sjba10815@gmail.com>
2025-07-10 16:14:03 +0900

    2025_07_10 완

--- inflearn/week5/1912.cpp
+++ inflearn/week5/1912.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
 #include <limits>
 using namespace std;
-int N{},a[100001],psum[100001],res{numeric_limits<int>::min()};
+int N{},a{},sum{},res{numeric_limits<int>::min()};
 
 int main(){
     cin >> N;
     for(int i{1}; i <= N; ++i){
-        cin >> a[i];
-        psum[i] = psum[i-1] + a[i];
+        cin >> a;
+        sum += a;
+        res = max(res, sum);
+        if(sum < 0 ) sum = 0;
     }
 
-    for(int interval{1}; interval <= N; ++interval){
-        for(int start{interval}; start <= N; ++start){
-            int sum = psum[start] - psum[start-interval];
-            res = max(res, sum);
-            if(interval == N) break;
-        }
-    }
     cout << res;
     return 0;
 }
